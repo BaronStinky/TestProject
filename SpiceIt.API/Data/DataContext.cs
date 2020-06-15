e553a2fec59f14266238bc053c0c11b6574d4fb7
BaronStinky <48348096+BaronStinky@users.noreply.github.com>
2020-06-15 16:47:24 +0200

    Initial Commit

---
+++ SpiceIt.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SpiceIt.API.Models;
+
+namespace SpiceIt.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
