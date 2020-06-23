3c031e8b98b11a7199d3da8e58485bb848f1a7e0
BaronStinky <48348096+BaronStinky@users.noreply.github.com>
2020-06-23 09:42:00 +0200

    End of Section 3

---
+++ SpiceIt.API/Dtos/UserForLoginDTO.cs
@@ -0,0 +1,9 @@
+namespace SpiceIt.API.Dtos
+{
+    public class UserForLoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
